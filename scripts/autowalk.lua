2fc5e539e8b592417baa71f111f61564e0e09d7b
amfero <65131540+amfero@users.noreply.github.com>
2022-02-02 15:50:25 +0300

    beeg skrept

---
+++ scripts/autowalk.lua
@@ -0,0 +1,20 @@
+--autowalk
+--author: amfero
+
+function main()
+
+	local autowalk = Module.new("AutoWalk", "xd", "MOVEMENT", this)
+
+	autowalk:body(function(mod)
+
+		autowalk:registerCallback("tick", function(tick)
+    		mc.options.keyForward:setPressed(true)
+		end)	
+
+		autowalk:onDisable(function()
+			mc.options.keyForward:setPressed(false)
+		end)
+
+	end)
+
+end
