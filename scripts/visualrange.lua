0c30780ad50cc7335d4ea82239b5b675ccb32c89
amfero <65131540+amfero@users.noreply.github.com>
2022-02-07 19:46:16 +0300

    Update visualrange.lua

--- scripts/visualrange.lua
+++ scripts/visualrange.lua
@@ -15,28 +15,31 @@ function main()
 
 			if(event:getName() == "tick") then	
 
-				if(mc.world == nil) then
-					known = {}
-				end
+				if(mc.world == nil) then known = {} end
 
-				local iterator = mc.world:getEntities():iterator()
-				while iterator:hasNext() do
-  					local entity = iterator:next()
-  					if(string.find(tostring(entity), "class_745")) then
-  						local ind = string.find(tostring(entity), "'")
-  						local ind1 = string.find(tostring(entity), "'/")
-  						local username = string.sub(tostring(entity), ind + 1, ind1 - 1)
-  						if(check(username) == false) then
-  							mc.inGameHud:getChatHud():addMessage(Text:of("§a+§f " .. username .. " §a> §7VisualRange"))
-  						end
-  					end
+				local entities = globals:getEntities()
+
+				for i = 0, entities:size() - 1 do
+
+					local username = entities:get(i):getEntityName()
+
+					if(#username < 32 and username ~= globals:getUsername()) then
+						
+						if(not check(username)) then mc.inGameHud:getChatHud():addMessage(Text:of("§a+§f " .. username .. " §a> §7VisualRange")) end
+
+					end
+
 				end
 
 				for i = 1, #known do
-					if(known[i] ~= nil and not string.find(tostring(mc.world:getEntities()), known[i])) then
+
+					if(known[i] ~= nil and not string.find(tostring(entities), known[i])) then
+
 						mc.inGameHud:getChatHud():addMessage(Text:of("§6-§f " .. known[i] .. " §6> §7VisualRange"))
 						table.remove(known, i)
+
 					end
+
 				end
 
 			end
@@ -44,16 +47,21 @@ function main()
 		end)
 
 		function check(username)
-			for i = 1, 1000 do
-				if(known[i] ~= nil and known[i] == username) then
-					return true
-				else
+
+			for i = 0, 1000 do
+
+				if(known[i] ~= nil and known[i] == username) then return true else
 					if(i > #known) then
+
 						table.insert(known, username)
 						return false
+					
 					end
+				
 				end
+			
 			end
+		
 		end
 
 	end)
