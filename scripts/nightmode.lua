7018c902b1b86cba156198dc160fd9ae0b76a919
amfero <65131540+amfero@users.noreply.github.com>
2022-03-16 20:18:33 +0300

    nightmode big upd

--- scripts/nightmode.lua
+++ scripts/nightmode.lua
@@ -8,11 +8,13 @@ function main()
     nightmode:body(function(mod)
 
         local time = NumberBuilder(15):name("Time"):setBounds(0, 23):build(nightmode)
+        local brightness = NumberBuilder(0.5):name("Brightness"):setBounds(0, 10):build(nightmode)
 
         nightmode:registerCallback("events", function(event)
         
             if(event:getName() == "tick") then
                 mc.world:setTimeOfDay(time:getValue() * 1000)
+                mc.options.gamma = brightness:getValue()
             end
 
             if(event:getName() == "packet_receive") then
